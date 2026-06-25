12141e495bb9682ffb4851baf8fb49cfdffca848
Johannes Hötter <62521561+jhoetter@users.noreply.github.com>
2026-06-25 06:28:26 +0000

    Adopt Sonaloop command palette

---
+++ packages/ui/src/sonaloop-command.ts
@@ -0,0 +1,8 @@
+export {
+  CommandPalette as SonaloopCommandPalette,
+  CommandPalettePanel as SonaloopCommandPalettePanel,
+  CommandTrigger as SonaloopCommandTrigger,
+  type CommandGroup as SonaloopCommandGroup,
+  type CommandItem as SonaloopCommandItem,
+  type CommandPaletteProps as SonaloopCommandPaletteProps,
+} from "sonaloop-design/components";
